f2bc733d1e9bd64d9453ee54395c9002221b5fc4
clement880101 <clement880101@gmail.com>
2018-10-06 16:39:18 -0400

    Permission for Camera coded

--- app/src/main/java/com/clementchang/hackgsu/MainActivity.java
+++ app/src/main/java/com/clementchang/hackgsu/MainActivity.java
@@ -1,13 +1,32 @@
 package com.clementchang.hackgsu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.hardware.camera2.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CAMERA_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},
+                    CAMERA_REQUEST );
+
+        }
+        
     }
+
+
 }
